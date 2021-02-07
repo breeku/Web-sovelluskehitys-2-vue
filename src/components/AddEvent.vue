21b7f61f924f1d2b76e0ab120feb8644c05a967c
breeku <bregfi@hotmail.com>
2021-02-07 17:30:03 +0200

    Tehtävät 5 ja 6

--- src/components/AddEvent.vue
+++ src/components/AddEvent.vue
@@ -1,20 +1,54 @@
 <template>
-    <p class="homeText">
-        Syötä tapahtuma
-    </p>
+    <div>
+        <p class="homeText">
+            Syötä tapahtuma
+        </p>
+        <br />
+        <form @submit.prevent="postEvent">
+            Tapahtuman nimi:
+            <input type="text" v-model="event" />
+            Tärkeä?
+            <input type="checkbox" v-model="important" />
+            <br />
+            <button type="postEvent">
+                Lähetä
+            </button>
+        </form>
+        <ul v-if="error && error.message">
+            {{
+                error.message
+            }}
+        </ul>
+    </div>
 </template>
 <script>
-    export default {
-    name: 'AddEvent'
-};
+import axios from "axios"
+export default {
+    name: "AddEvent",
+    data() {
+        return {
+            event: "",
+            important: false,
+            error: null,
+        }
+    },
+    methods: {
+        async postEvent() {
+            await axios.post("http://localhost:3000/notes", {
+                content: this.event,
+                important: this.important,
+            })
+        },
+    },
+}
 </script>
 <style scoped>
-    .homeText{
+.homeText {
     font-size: 35px;
     color: red;
     text-align: center;
     position: relative;
-    top:30px;
+    top: 30px;
     text-shadow: 2px 2px 2px gray;
 }
 </style>
--- src/components/ListEvent.vue
+++ src/components/ListEvent.vue
@@ -1,20 +1,44 @@
 <template>
-  <p class="homeText">
-    Listaa tapahtumia
-  </p>
+    <div>
+        <p class="homeText">
+            Listaa tapahtumia
+        </p>
+        <br />
+        <ul id="notes">
+            <li v-for="note in notes" :key="note.id">
+                {{ note.content }}. Tärkeä:
+                {{ note.important ? "Kyllä" : "Ei" }}
+            </li>
+        </ul>
+    </div>
 </template>
 <script>
+import axios from "axios"
 export default {
-  name: 'ListEvent'
-};
+    name: "ListEvent",
+    data() {
+        return {
+            notes: [],
+            error: null,
+        }
+    },
+    async created() {
+        try {
+            const { data } = await axios.get("http://localhost:3000/notes")
+            this.notes = data
+        } catch (e) {
+            this.error = e
+        }
+    },
+}
 </script>
 <style scoped>
-.homeText{
-  font-size: 35px;
-  color: red;
-  text-align: center;
-  position: relative;
-  top:30px;
-  text-shadow: 2px 2px 2px gray;
+.homeText {
+    font-size: 35px;
+    color: red;
+    text-align: center;
+    position: relative;
+    top: 30px;
+    text-shadow: 2px 2px 2px gray;
 }
 </style>
